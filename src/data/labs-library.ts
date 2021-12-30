0b92ed002847724a96650729bfc7ab0b6c5baa1e
utbrott <rosinskimk@gmail.com>
2021-12-31 00:52:37 +0100

    feat(library): change lorem ipsum to description

--- src/data/labs-library.ts
+++ src/data/labs-library.ts
@@ -4,21 +4,16 @@ export const cardItems: CardItem[] = [
   {
     id: 'temperature',
     title: 'Temperature sensors: RTD & Thermocouple',
-    desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, magni. Dignissimos, minima sunt maiores est ullam voluptates eligendi et earum illum perspiciatis sint esse maxime in necessitatibus eaque rerum illo ipsum voluptatum? Blanditiis molestiae quisquam sapiente perspiciatis praesentium illum totam reiciendis, consequatur consectetur, rem excepturi eos! Facilis asperiores ducimus aut.',
+    desc: 'Aim of this laboratory is to observe how the temperature sensors parameters change dependant on the configuration. For each sensor type there are two tasks to be completed. Using the provided values and formulas complete the tasks and view the generated graph. ',
   },
   {
     id: 'displacement',
     title: 'Displacement sensors: LVDT',
-    desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, magni. Dignissimos, minima sunt maiores est ullam voluptates eligendi et earum illum perspiciatis sint esse maxime in necessitatibus eaque rerum illo ipsum voluptatum? Blanditiis molestiae quisquam sapiente perspiciatis praesentium illum totam reiciendis, consequatur consectetur, rem excepturi eos! Facilis asperiores ducimus aut.',
+    desc: 'Aim of this laboratory is to observe how the LVDT displacement sensor parameters change dependant on the configuration. There is one task to be completed. Using the provided values and formulas complete the tasks and view the generated graph. ',
   },
   {
     id: 'strain',
     title: 'Strain gauge sensors',
-    desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, magni. Dignissimos, minima sunt maiores est ullam voluptates eligendi et earum illum perspiciatis sint esse maxime in necessitatibus eaque rerum illo ipsum voluptatum? Blanditiis molestiae quisquam sapiente perspiciatis praesentium illum totam reiciendis, consequatur consectetur, rem excepturi eos! Facilis asperiores ducimus aut.',
-  },
-  {
-    id: 'piezoelectric',
-    title: 'Piezoelectric sensors',
-    desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, magni. Dignissimos, minima sunt maiores est ullam voluptates eligendi et earum illum perspiciatis sint esse maxime in necessitatibus eaque rerum illo ipsum voluptatum? Blanditiis molestiae quisquam sapiente perspiciatis praesentium illum totam reiciendis, consequatur consectetur, rem excepturi eos! Facilis asperiores ducimus aut.',
+    desc: 'Aim of this laboratory is to observe how the strain gauge sensor parameters change dependant on the configuration. There are two tasks to be completed. Using the provided values and formulas complete the tasks and view the generated graph. ',
   },
 ];

7d4c3e5665efe5f41310d2515fb9a2c3b122e817
utbrott <rosinskimk@gmail.com>
2022-12-24 21:52:14 +0100

    refactor: components structure

---
+++ src/components/atoms/theme-toggle.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import { Button } from '../ui/button';
+import { SunIcon, MoonIcon } from '@heroicons/react/24/solid';
+
+export function ThemeToggle() {
+  const [isMounted, setIsMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const t = theme === 'light' ? 'dark' : 'light';
+
+  return isMounted ? (
+    <Button
+      variant='default'
+      modifier='ghost'
+      iconOnly
+      onClick={() => setTheme(t)}
+    >
+      {theme === 'light' ? (
+        <SunIcon className='w-5 h-5' />
+      ) : (
+        <MoonIcon className='w-5 h-5' />
+      )}
+    </Button>
+  ) : null;
+}

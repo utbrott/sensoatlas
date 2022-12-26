6ad4b4bba226a57e25de097ee254c09ed507a173
utbrott <rosinskimk@gmail.com>
2022-12-26 03:43:35 +0100

    fix(prettier,eslint): add missing configs

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+  plugins: [require('prettier-plugin-tailwindcss')]
+}

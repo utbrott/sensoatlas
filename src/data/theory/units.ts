489a11ae0b42786f7f8a1639aa39ca668b358883
utbrott <rosinskimk@gmail.com>
2022-12-29 22:05:43 +0100

    refactor(data/units): move units to separete file

---
+++ src/data/theory/units.ts
@@ -0,0 +1,15 @@
+export const units = {
+  /* General */
+  ohms: '$\\big[\\Omega\\big]$',
+  kohms: '$\\big[k\\Omega\\big]$',
+  density: '$\\big[kg{\\cdot}m^{-3}\\big]$',
+  /* Temperture */
+  celcius: '\u00b0C',
+  heatCapacity: '$\\big[J{\\cdot}(kg\\degree{C})^{-1}\\big]$',
+  thermalConductivity: '$\\big[W{\\cdot}(mK)^{-1}\\big]$',
+  seebeckCoeff: '$\\big[\\mu{V}{\\cdot}K^{-1}\\big]',
+  /* Strain */
+  microstrains: '$\\big[\\mu\\varepsilon\\big]$',
+  /* Electromagnetism */
+  permeability: '$\\big[H{\\cdot}m^{-1}'
+}

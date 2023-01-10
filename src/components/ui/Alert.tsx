187da86b2ad81315eca5642ef5044c9e84d430eb
utbrott <rosinskimk@gmail.com>
2023-01-10 12:13:52 +0100

    refactor(alerts): update styling (fix contrast)

--- src/components/ui/Alert.tsx
+++ src/components/ui/Alert.tsx
@@ -7,14 +7,14 @@ import {
 } from '@heroicons/react/20/solid'
 
 const alert = cva(
-  'flex items-start justify-center min-h-fit rounded-md p-4 border gap-4 bg-opacity-20 dark:bg-opacity-10',
+  'flex items-center justify-center min-h-fit rounded-md p-4 space-x-4',
   {
     variants: {
       status: {
-        error: 'bg-red-600 border-red-600/20',
-        success: 'bg-green-600 border-green-600/20',
-        warning: 'bg-yellow-600 border-yellow-600/20',
-        info: 'bg-sky-600 border-sky-600/20'
+        error: 'bg-red-400/20 dark:bg-red-400/10',
+        success: 'bg-green-400/20 dark:bg-green-400/10',
+        warning: 'bg-yellow-400/20 dark:bg-yellow-300/10',
+        info: 'bg-sky-300/20 dark:bg-sky-400/10'
       },
       fullWidth: {
         true: 'w-full',
@@ -52,7 +52,7 @@ const alert = cva(
   }
 )
 
-const alertIcon = cva('w-5 h-5', {
+const alertIcon = cva('w-6 h-6', {
   variants: {
     status: {
       error: 'text-red-500 ',
@@ -66,10 +66,10 @@ const alertIcon = cva('w-5 h-5', {
 const alertTitle = cva('h-5 w-full font-semibold text-sm tracking-wide', {
   variants: {
     status: {
-      error: 'text-red-700 dark:text-red-500/90',
-      success: 'text-green-700 dark:text-green-500/90',
-      warning: 'text-yellow-700 dark:text-yellow-500/90',
-      info: 'text-sky-700 dark:text-sky-600'
+      error: 'text-red-600 dark:text-red-500',
+      success: 'text-green-600 dark:text-green-500',
+      warning: 'text-yellow-600 dark:text-yellow-500',
+      info: 'text-sky-600 dark:text-sky-500'
     }
   }
 })
@@ -105,13 +105,13 @@ export const Alert = ({
 }: AlertProps) => {
   return (
     <div className={alert({ status, fullWidth, accentBorder })}>
-      <div className='flex items-start justify-start gap-4'>
+      <div className='flex items-center justify-center gap-4'>
         <AlertIcon status={status} />
       </div>
       <div className='flex w-full flex-col'>
         {title && <div className={alertTitle({ status })}>{title}</div>}
         {children && (
-          <div className='w-full text-sm text-gray-700 dark:text-gray-300'>
+          <div className='w-full text-sm text-gray-700 dark:text-gray-200'>
             {children}
           </div>
         )}

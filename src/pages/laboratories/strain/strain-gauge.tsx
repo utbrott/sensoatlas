fc5a50d758de4b698b5921dbf63fb80a0f7b143b
utbrott <rosinskimk@gmail.com>
2023-01-16 21:09:34 +0100

    feat: add all strain charts

--- src/pages/laboratories/strain/strain-gauge.tsx
+++ src/pages/laboratories/strain/strain-gauge.tsx
@@ -13,6 +13,8 @@ import { Tasks } from '@atoms/tasks'
 import { taskFields } from '@data/laboratories/strain/strain-gauge'
 import { lineChartCreator } from '@atoms/chart'
 import { LineChart } from '@atoms/chart'
+import { Tab } from '@ui/tab-group'
+import { getNewGaugeResistance } from '@data/index'
 
 // const Lab = () => {
 //   const [labState, dispatch] = useReducer(labStateReducer, initialLabState)
@@ -57,26 +59,89 @@ export default function StrainGauge() {
 interface Charts {}
 
 const Charts = () => {
-  const [store] = useStore((store: Record<string, number[]>) => store)
+  const [dataStore] = useStore((store: Record<string, number[]>) => store)
+  const [configStore] = useStore(
+    (store: Record<string, { [key: string]: string | number }>) => store
+  )
 
-  const chartData = lineChartCreator({
-    xvalues: store.data0,
-    yvalues: store.validation0
+  const chart1Data = lineChartCreator({
+    xvalues: dataStore.data0,
+    yvalues: dataStore.validation0
   })
 
-  console.log('chartdata', chartData)
+  const chart2Data = lineChartCreator({
+    xvalues: dataStore.data1,
+    yvalues: dataStore.validation1
+  })
 
-  return chartData.length > 0 ? (
-    <div>
-      <LineChart
-        chartName='strain-out-volt'
-        chartData={chartData}
-        labels={{
-          xaxis: 'Microstrains [\u00b5\u03b5]',
-          yaxis: 'Output voltage [mV]'
-        }}
-      />
-    </div>
+  const chart3values = getNewGaugeResistance({
+    material: {
+      gaugeFactor: Number(configStore.material.gaugeFactor),
+      modulus: Number(configStore.material.modulus),
+      tempCoeff: Number(configStore.material.tempCoeff)
+    },
+    resistance: Number(configStore.resistance.resistance),
+    bridge: {
+      name: String(configStore.bridge.name),
+      multiplier: Number(configStore.bridge.multiplier)
+    },
+    taskData: dataStore.data1
+  })
+
+  const chart3Data = lineChartCreator({
+    xvalues: dataStore.data1,
+    yvalues: chart3values
+  })
+
+  const dataReady =
+    chart1Data.length > 0 && chart2Data.length > 0 && chart3Data.length > 0
+
+  return dataReady ? (
+    <Tab.Group>
+      <Tab.List>
+        <Tab>Task 1</Tab>
+        <Tab>Task 2.1</Tab>
+        <Tab>Task 2.2</Tab>
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>
+          <LineChart
+            chartName='strain-out-volt'
+            chartData={chart1Data}
+            labels={{
+              xaxis: 'Microstrains [\u00b5\u03b5]',
+              yaxis: 'Output voltage [mV]'
+            }}
+            hasDataPoints
+            withTooltip
+          />
+        </Tab.Panel>
+        <Tab.Panel>
+          <LineChart
+            chartName='temperature-out-voltage'
+            chartData={chart2Data}
+            labels={{
+              xaxis: 'Microstrains [\u00b5\u03b5]',
+              yaxis: 'Output voltage [mV]'
+            }}
+            hasDataPoints
+            withTooltip
+          />
+        </Tab.Panel>
+        <Tab.Panel>
+          <LineChart
+            chartName='temperature-resistance'
+            chartData={chart3Data}
+            labels={{
+              xaxis: 'Microstrains [\u00b5\u03b5]',
+              yaxis: 'Output voltage [mV]'
+            }}
+            hasDataPoints
+            withTooltip
+          />
+        </Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
   ) : null
 }
 

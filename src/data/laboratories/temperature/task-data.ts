bb216e4311bac4a1eb16efcd1743f28d1cccaa9a
utbrott <rosinskimk@gmail.com>
2023-01-28 22:12:21 +0100

    feat(labs/temperature): dynamic characteristic slope generation

--- src/data/laboratories/temperature/task-data.ts
+++ src/data/laboratories/temperature/task-data.ts
@@ -19,3 +19,83 @@ export const getRandomTemperatureSet = ({
     { lower: min - 5, upper: max + 5 }
   )
 }
+
+type SensorType = 'rtd' | 'thermocouple'
+
+interface GetTemperatureSlopes {
+  sensor: SensorType
+  timeConstantValues: number[]
+}
+
+interface GetTemperatureValue {
+  time: number
+  timeConstantValue: number
+  sensor: SensorType
+}
+
+export const getTemperatureSlopes = ({
+  sensor,
+  timeConstantValues
+}: GetTemperatureSlopes) => {
+  const getTemperatureValue = ({
+    time,
+    timeConstantValue,
+    sensor
+  }: GetTemperatureValue) => {
+    const INITIAL_TEMPERATURE = 0
+    const surroudingTemperature = sensor === 'rtd' ? 50 : 300
+
+    return round(
+      surroudingTemperature -
+        (surroudingTemperature - INITIAL_TEMPERATURE) *
+          Math.exp((time / timeConstantValue) * -1),
+      2
+    )
+  }
+
+  const bareTimeConstantSlope: number[] = []
+  const sheathedTimeConstantSlope: number[] = []
+  const thermowellTimeConstantSlope: number[] = []
+
+  const SEC_IN_MIN = 60
+  const time: number[] = []
+
+  for (let i = 0; i <= SEC_IN_MIN; i++) {
+    time[i] = i
+  }
+
+  time.forEach(tick => {
+    bareTimeConstantSlope.push(
+      getTemperatureValue({
+        time: tick,
+        timeConstantValue: timeConstantValues[0],
+        sensor: sensor
+      })
+    )
+
+    sheathedTimeConstantSlope.push(
+      getTemperatureValue({
+        time: tick,
+        timeConstantValue: timeConstantValues[1],
+        sensor: sensor
+      })
+    )
+
+    thermowellTimeConstantSlope.push(
+      getTemperatureValue({
+        time: tick,
+        timeConstantValue: timeConstantValues[2],
+        sensor: sensor
+      })
+    )
+  })
+
+  return {
+    xvalue: time,
+    yvalues: [
+      bareTimeConstantSlope,
+      sheathedTimeConstantSlope,
+      thermowellTimeConstantSlope
+    ]
+  }
+}

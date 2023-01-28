f89c1f6c7eb6d9f2fda3ba87f6541a23f73e16e8
utbrott <rosinskimk@gmail.com>
2023-01-28 01:23:32 +0100

    feat(labs/temperature): all temperature lab data functions

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,5 @@
-export * from './laboratories/strain/strain-gauge'
 export * from './laboratories/strain/task-data'
 export * from './laboratories/strain/validation-data'
 
-export * from './laboratories/temperature/rtd'
-export * from './laboratories/temperature/themocouple'
 export * from './laboratories/temperature/task-data'
+export * from './laboratories/temperature/validation-data'

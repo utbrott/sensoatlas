0d5c2c157c8ffa0277c74abdcc7e62a509f57959
utbrott <rosinskimk@gmail.com>
2023-01-28 02:11:16 +0100

    feat(labs/lvdt): partial lvdt lab, config store

---
+++ src/store/lvdt.ts
@@ -0,0 +1,14 @@
+import { createStoreContext } from '@store/create-store-context'
+
+import { configFields, taskFields } from '@data/laboratories/temperature/rtd'
+
+import { initialConfigCreator } from '@atoms/config'
+import { initialTaskCreator, initialValidationCreator } from '@atoms/tasks'
+
+export const config = initialConfigCreator({ fields: configFields })
+export const tasks = initialTaskCreator({ fields: taskFields })
+export const validation = initialValidationCreator({ fields: taskFields })
+
+const testContext = { ...config, ...tasks, ...validation }
+
+export const { Provider, useStore } = createStoreContext(testContext)
